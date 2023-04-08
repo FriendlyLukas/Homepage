80a1489d2d868f4aa6cc7f4069339e2ae477bd1c
Łukasz Palow <lukaspalow@gmail.com>
2023-04-08 13:23:02 +0200

    Merge branch 'main' of https://github.com/FriendlyLukas/Home-pagegit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let przycisk = document.querySelector(".przycisk");
 let body = document.querySelector(".body");
 let themeName = document.querySelector(".themeName");
@@ -8,3 +9,15 @@ przycisk.addEventListener("click", () => {
   themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
 
 });
+=======
+let przycisk = document.querySelector(".przycisk");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+
+przycisk.addEventListener("click", () => {
+  body.classList.toggle("body--dark");
+
+  themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+
+});
+>>>>>>> f27654633005332bc320d79a59c47c80ba8692f3
