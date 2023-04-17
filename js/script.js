e50505983ba72317bc46c12a3fd80fde0325abd2
Łukasz Palow <lukaspalow@gmail.com>
2023-04-17 18:31:38 +0200

    small update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let przycisk = document.querySelector(".przycisk");
 let body = document.querySelector(".body");
 let themeName = document.querySelector(".themeName");
@@ -9,15 +8,3 @@ przycisk.addEventListener("click", () => {
   themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
 
 });
-=======
-let przycisk = document.querySelector(".przycisk");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
-
-przycisk.addEventListener("click", () => {
-  body.classList.toggle("body--dark");
-
-  themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
-
-});
->>>>>>> f27654633005332bc320d79a59c47c80ba8692f3
