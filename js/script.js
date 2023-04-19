06bc6e7b0109114da331d479a1838546b7e65d8f
Łukasz Palow <lukaspalow@gmail.com>
2023-04-19 18:38:33 +0200

    Fuctions order performed in script

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-let przycisk = document.querySelector(".przycisk");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
+{
+const welcome = () => {
+  console.log("Witam serdecznie wszystkie/ch deweloperki i deweloperów, którzy wstąpili na moją witrynę internetową")
+}
 
-przycisk.addEventListener("click", () => {
+const toogleBackground = () => {
+
+  const themeName = document.querySelector(".themeName");
+  const body = document.querySelector(".body");
+
   body.classList.toggle("body--dark");
-
   themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+};
 
-});
+
+const init = () =>{
+  
+  const changeBackgroundButton = document.querySelector(".changeBackgroundButton");
+
+  changeBackgroundButton.addEventListener("click", toogleBackground);
+  
+  welcome();
+}
+
+init();
+
+}
