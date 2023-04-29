3d653a5fa2a56129ca4080a998df3347ecbefb96
Łukasz Palow <lukaspalow@gmail.com>
2023-04-29 14:40:35 +0200

    JS-cod-order-correction

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 {
-const welcome = () => {
-  console.log("Witam serdecznie wszystkie/ch deweloperki i deweloperów, którzy wstąpili na moją witrynę internetową")
-}
+  const welcome = () => {
+    console.log("Witam serdecznie wszystkie/ch deweloperki i deweloperów, którzy wstąpili na moją witrynę internetową")
+  }
 
-const toogleBackground = () => {
+  const toogleBackground = () => {
 
-  const themeName = document.querySelector(".themeName");
-  const body = document.querySelector(".body");
+    const themeName = document.querySelector(".themeName");
+    const body = document.querySelector(".body");
 
-  body.classList.toggle("body--dark");
-  themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
-};
+    body.classList.toggle("body--dark");
+    themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+  };
 
+  const init = () =>{
+    
+    const changeBackgroundButton = document.querySelector(".changeBackgroundButton");
 
-const init = () =>{
-  
-  const changeBackgroundButton = document.querySelector(".changeBackgroundButton");
+    changeBackgroundButton.addEventListener("click", toogleBackground);
+    
+    welcome();
+  }
 
-  changeBackgroundButton.addEventListener("click", toogleBackground);
-  
-  welcome();
-}
-
-init();
+  init();
 
 }
